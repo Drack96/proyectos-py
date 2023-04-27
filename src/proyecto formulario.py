622c6daf6660e81db3c98fb1d8928574b76a6754
Cristian Quintero <crquintero.03@gmail.com>
2023-04-27 11:05:30 -0400

    commit inicial

---
+++ src/proyecto formulario.py
@@ -0,0 +1,18 @@
+str= nombre = str
+str= apellido = str
+str= Dato = str
+int= num1= int
+str= ciudad = str
+int= num2 = int
+
+print("¡¡¡Bienvenido al sistema de informacion KIOCERA!!!")
+print("por favor indicar los siguientes datos.")
+print("//////////////////////**////////////////////")
+nombre= str(input("Nombre: "))
+apellido= str(input("Apellido: "))
+Dato= str(input("Tipo de Documento: "))
+num1= int(input("Escribe numero de Documento: "))
+ciudad= str(input("ciudad de donde nacio: "))
+num2= int(input("Indique que edad tiene: "))
+print("//////////////////////**////////////////////")
+
---
+++ src/proyecto hola mundo.py
@@ -0,0 +1,19 @@
+print ("hola mundo")
+
+int = num1 = 5
+int = num2 = 10
+
+print(num1)
+print(num2)
+print(num1+num2)
+
+print([num2] + [num1]+ [num1+num2])
+
+if num1 <= num2:
+    print("este numero es menor")
+    
+else:
+    ("este numero es meyor" )
+print("hasta luego!!!")
+
+
